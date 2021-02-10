776125ed82efedbdde2d0fc109c2223a43bce15d
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-09 21:32:28 -0300

    pagina index no servidor ok

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,6 +3,8 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.use(express.static('public'))
+
 server.set("view engine", "html")
 
 nunjucks.configure("views", {
@@ -10,7 +12,7 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req, res) {
-    return res.send("Show")
+    return res.render("index")
 })
 
 server.listen(5000, function() {
