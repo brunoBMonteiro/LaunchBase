955f05290b2ad699cf5e95c050571eb39ea27f2f
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-11 15:47:23 -0300

    nunjucks instalado, template engine

---
+++ Back-end/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "dark-mode-com-css-mudando-a-aparencia-do-blog-de-maneira-simples-e-rapida",
+        title: "Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida",
+        author: "Por Thiago Marinho",
+        duration: "3 MIN"
+    },
+    {
+        id: "construindo-um-app-com-mapas-usando-react-native-maps-e-mapbox",
+        title: "Mapas com React usando Leaflet",
+        author: "Por Thiago Marinho",
+        duration: "8 MIN"
+    },
+    {
+        id: "dark-mode-com-css-mudando-a-aparencia-do-blog-de-maneira-simples-e-rapida",
+        title: "Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida",
+        author: "Por Thiago Marinho",
+        duration: "3 MIN"
+    }
+]
--- Back-end/server.js
+++ Back-end/server.js
@@ -2,17 +2,26 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const blogs = require("./data")
 
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server
 })
 
 server.get("/", function(req, res) {
-    return res.render("index")
+    return res.render("about")
+})
+
+server.get("/home", function(req, res) {
+    return res.render("home")
+})
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio")
 })
 
 server.listen(5000, function() {
